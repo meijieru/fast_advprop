de5fa50ff734bee963931834b542934346bc69b3
meijieru <meijieru@gmail.com>
2022-03-20 21:18:57 -0700

    chore(format): sh editorconfig

--- scripts/run_fastadvprop.sh
+++ scripts/run_fastadvprop.sh
@@ -13,20 +13,20 @@ shared_params="--attack-iter ${attack_iter} --attack-epsilon ${attack_eps} --att
 postfix=_batch${train_batch}_lr${lr}
 
 function run_fast_advprop() {
-	multi_clean_strategy=4:1
-	lr_strategy=shared:1,clean:1,adv:1
-	loss_strategy=clean:1,attack:0.5,adv:0.5
+    multi_clean_strategy=4:1
+    lr_strategy=shared:1,clean:1,adv:1
+    loss_strategy=clean:1,attack:0.5,adv:0.5
 
-	python imagenet.py ${shared_params} \
-		--gpu-id ${GPU_ID} \
-		-c exp/imagenet/epoch${run_epochs}/${model}-fastadvprop_mcs.${multi_clean_strategy}_ls.${lr_strategy}_los.${loss_strategy}_${postfix} \
-		--attacker_type pgd \
-		--multi_clean_strategy ${multi_clean_strategy} \
-		--lr_strategy ${lr_strategy} --loss_strategy ${loss_strategy} \
-		--reuse_mid_grad --attack_in_train \
-		--original_attack \
-		--exact_same_training_budget \
-		--shuffle_before_train_adv
+    python imagenet.py ${shared_params} \
+        --gpu-id ${GPU_ID} \
+        -c exp/imagenet/epoch${run_epochs}/${model}-fastadvprop_mcs.${multi_clean_strategy}_ls.${lr_strategy}_los.${loss_strategy}_${postfix} \
+        --attacker_type pgd \
+        --multi_clean_strategy ${multi_clean_strategy} \
+        --lr_strategy ${lr_strategy} --loss_strategy ${loss_strategy} \
+        --reuse_mid_grad --attack_in_train \
+        --original_attack \
+        --exact_same_training_budget \
+        --shuffle_before_train_adv
 }
 
 run_fast_advprop
